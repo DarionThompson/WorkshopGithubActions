982fd2219e429bf4eeebe776e93e6b2c4a52e21f
Darion Thompson <darion@stepquest.com>
2022-04-28 19:35:07 -0400

    updated actions

--- BabySitterKata/BabySitterKataTests/BabySitterTestsShould.cs
+++ BabySitterKata/BabySitterKataTests/BabySitterTestsShould.cs
@@ -61,5 +61,162 @@ namespace BabySitterKataTests
             //Assert
             Assert.Contains(selectAFamilyMessage, earnings);
         }
+
+       [Fact]
+        public void IfTheclockoutTimeIsLaterThan4AMAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            _clockOutTime = "5AM";
+            var lateClockOutTimeMessage = "You cannout work past 4AM";
+
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(lateClockOutTimeMessage, earnings);
+        }
+
+       [Fact]
+        public void IfTheclockoutTimeIsBeforeThanClockInTimeAnErrorValidationMessageIsReturned()
+        {
+            //Arrange
+            _clockInTime = "8PM";
+            _clockOutTime = "6PM";
+            var errorClockOutTimeMessage = "Your end time cannot be before your start time";
+
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(errorClockOutTimeMessage, earnings);
+        }
+
+       [Fact]
+        public void IfTheBabySitterDoesNotSelectAClockInTimeASelectClockInTimeErrorValidationmessageIsReturned()
+        {
+            //Arrange
+            _clockInTime = string.Empty;
+
+            var selectAClockInTimeMessage = "You must select a start time";
+
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(selectAClockInTimeMessage, earnings);
+        }
+
+       [Fact]
+        public void IfTheBabySitterDoesNotSelectAClockOutTimeASelectClockOutTimeErrorValidationmessageIsReturned()
+        {
+            //Arrange
+            _clockOutTime = string.Empty;
+
+            var selectAClockOutTimeMessage = "You must select an end time";
+
+            //Act
+            var earnings = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(selectAClockOutTimeMessage, earnings);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyAAndWorksBetween5PMAnd11PMAValueOf90IsReturned()
+        {
+            //Arrange
+            var expectedPay = "90";
+            _clockOutTime = "11PM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyAAndWorksBetween5PMAnd4AMAValueOf190IsReturned()
+        {
+            //Arrange
+            var expectedPay = "190";
+            _clockOutTime = "4AM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceA).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyBAndWorksBetween5PMAnd10PMAValueOf60IsReturned()
+        {
+            //Arrange
+            var expectedPay = "60";
+            _clockOutTime = "10PM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceB).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyBAndWorksBetween5PMAnd12PMAValueOf76IsReturned()
+        {
+            //Arrange
+            var expectedPay = "76";
+            _clockOutTime = "12AM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceB).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyBAndWorksBetween5PMAnd4AMAValueOf140IsReturned()
+        {
+            //Arrange
+            var expectedPay = "140";
+            _clockOutTime = "4AM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceB).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyCAndWorksBetween5PMAnd9PMAValueOf84IsReturned()
+        {
+            //Arrange
+            var expectedPay = "84";
+            _clockOutTime = "9PM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceC).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
+
+       [Fact]
+        public void IfTheBabySitterSelectsFamilyCAndWorksBetween5PMAnd41MAValueOf189IsReturned()
+        {
+            //Arrange
+            var expectedPay = "189";
+            _clockOutTime = "4AM";
+
+            //Act
+            var actualPay = _babySitter.CalculateNightlyCharge(_clockInTime, _clockOutTime, _familyChoiceC).ToList();
+
+            //Assert
+            Assert.Contains(expectedPay, actualPay);
+        }
     }
 }
